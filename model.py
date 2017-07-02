d646eec6f1c5c3b0220021fe21c10f19ebef7a7f
Peter Schoener <peter.c.schoener@gmail.com>
2017-07-02 14:27:23 +0200

    it works now!

--- model.py
+++ model.py
@@ -8,18 +8,43 @@ class Phase(Enum):
 	Predict = 2
 
 class Model:
-	def __init__(self, batch, label_batch, n_chars, hidden_sizes = [], phase=Phase.Predict, use_char_embeds = True) :
+	def __init__(self, batch, label_batch, n_chars, hidden_sizes = [], phase = Phase.Predict, use_char_embeds = True) :
 		batch_size = batch.shape[1]
 		input_size = batch.shape[2]
 		label_size = label_batch.shape[2]
 
-		self._x = tf.placeholder(tf.int32, shape=[batch_size, input_size])
+		self._x = tf.placeholder(tf.int32, shape = [batch_size, input_size])
 
 		if phase != Phase.Predict:
-			self._y = tf.placeholder(tf.float32, shape=[batch_size, label_size])
+			self._y = tf.placeholder(tf.float32, shape = [batch_size, label_size])
 
-		# Fixme: Your code goes here!
-		raise NotImplementedError
+		# build hidden layers
+		layer = tf.reshape(tf.one_hot(self._x, n_chars), [batch_size, -1])
+		for (i, hidden_size) in enumerate(hidden_sizes):
+			W = tf.get_variable("W_hidden_%d" % i, shape = [layer.shape[1], hidden_size])
+			b = tf.get_variable("b_hidden_%d" % i, shape = [hidden_size])
+			hidden_outputs = tf.sigmoid(tf.matmul(layer, W) + b)
+			layer = hidden_outputs
+		hidden = layer
+		
+		# build output layer
+		w = tf.get_variable("w", shape = [hidden.shape[1], label_size])
+		b = tf.get_variable("b", shape = [1])
+		
+		# create loss function
+		logits = tf.matmul(hidden, w) + b
+		losses = tf.nn.sigmoid_cross_entropy_with_logits(labels = tf.cast(self._y, tf.float32), logits = logits)
+		self._loss = tf.reduce_sum(losses)
+		
+		if phase == Phase.Train :
+			self._train_op = tf.train.AdamOptimizer(0.004).minimize(self._loss) # converges much faster than gradient descent
+		else :
+			self._probs = tf.sigmoid(logits)
+			self._labels = tf.cast(tf.round(self._probs), tf.int64)
+			
+			if phase == Phase.Validation :
+				correct = tf.equal(tf.argmax(self._y), tf.argmax(self._labels)) # argmax because there are potentially multiple labels per token
+				self._accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
 
 	@property
 	def accuracy(self):
